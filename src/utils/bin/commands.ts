345517b1042fed2447274dfbec01bb6d32fa0657
Yiğit Küçük <yigitkucukbusiness@gmail.com>
2023-01-08 21:15:05 +0300

    Update commands.ts

--- src/utils/bin/commands.ts
+++ src/utils/bin/commands.ts
@@ -139,7 +139,7 @@ export const banner = (args?: string[]): string => {
   ░░░░░░          ░░░░░░                                                                       
 
 Type 'help' to see the list of available commands.
-Type 'sumfetch' to display information about me.
-Type 'repo' or click <u><a class="text-light-blue dark:text-dark-blue underline" href="${config.repo}" target="_blank">here</a></u> for the Github repository.
+Type 'sumfetch' to display information about my handles.
+Type 'repo' for the Github repository.
 `;
 };
