ef947e492d4f5564f7c0683d53d13ac1ae81fea7
Yiğit Küçük <yigitkucukbusiness@gmail.com>
2023-01-08 21:02:05 +0300

    Update sumfetch.ts

--- src/utils/bin/sumfetch.ts
+++ src/utils/bin/sumfetch.ts
@@ -23,21 +23,22 @@ const sumfetch = async (args: string[]): Promise<string> => {
 `;
   } else {
     return `
-        _ __/\
-    /\_\      \                                    sumfetch: summary display
-   |           \                                  -----------
-   |             \           ___                   ABOUT
-    \             \        /  _ \                  ${config.name}
-   _|              \      {  ( \^)                ﰩ ${config.ps1_hostname}
-   \                \     \  \  \|                 <u><a href="${config.resume_url}" target="_blank">resume</a></u>
-    \                \     \  \                   爵 <u><a href="${config.repo}" target="_blank">Github repo</a></u>
-    --                \ ____)  \                  -----------
-   \                            \                  CONTACT 
-    |                           )                  <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u>
-  ___\_________________________/____________       <u><a href="https://github.com/${config.social.github}" target="_blank">github.com/${config.social.github}</a></u>
-     ..........................                    <u><a href="https://linkedin.com/in/${config.social.linkedin}" target="_blank">linkedin.com/in/${config.social.linkedin}</a></u>
-    ............................
-
+             @@@@@@@@@@@@@                   sumfetch: summary display
+        @@@@               @@@@             -----------
+      @@                       @@            ABOUT
+    @@                           @@          ${config.name}
+  @@                               @@       ﰩ ${config.ps1_hostname}
+ @@                         @@@     @@       <u><a href="${config.resume_url}" target="_blank">resume</a></u>
+@@        @@@                        @@     爵 <u><a href="${config.repo}" target="_blank">Github repo</a></u>
+@@                                   @@     -----------
+@@             .@@@@@@@@@@.          @@      CONTACT 
+ @@           @@          @@        @@       <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u>
+  @@           @@        @@        @@        <u><a href="https://github.com/${config.social.github}" target="_blank">github.com/${config.social.github}</a></u>
+   @@             @@@@@@          @@         <u><a href="https://linkedin.com/in/${config.social.linkedin}" target="_blank">linkedin.com/in/${config.social.linkedin}</a></u>
+     @@@                        @@@         -----------
+        @@@                  @@@ @@          DONATE 
+         @|  @@@@@@@@@@@@@@@@   @@           <u><a href="${config.donate_urls.paypal}" target="_blank">${config.donate_urls.paypal}</a></u>
+         @|                      @@          <u><a href="${config.donate_urls.patreon}" target="_blank">${config.donate_urls.patreon}</a></u>
 `;
   }
 };
